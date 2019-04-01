1b21095f807eee6e65e9620b24b1d20fb96c076d
ImHoJeong <39ghwjd@naver.com>
2019-04-02 01:53:53 +0900

    Add 3.4.md file

---
+++ Chapter3/func.js
@@ -0,0 +1,12 @@
+const { odd, even } = require('./var');
+
+function checkOddOrEven(num) {
+    if (num % 2) { // 홀수면
+        return odd;
+    } else {
+        return even;
+    }
+
+}
+
+module.exports = checkOddOrEven;
---
+++ Chapter3/index.js
@@ -0,0 +1,14 @@
+const { odd, even } = require('./var');
+const checkNumber = require('./func');
+
+function checkStringOddOrEven(str) {
+    if (str.length % 2) // 홀수면
+    {
+        return odd;
+    } else {
+        return even;
+    }
+}
+
+console.log(checkNumber(10));
+console.log(checkStringOddOrEven('hello'));
