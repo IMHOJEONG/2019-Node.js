092d7a35afba6ad6e76dc5cdb0b6dffd27430855
ImHoJeong <39ghwjd@naver.com>
2019-04-21 14:15:31 +0900

    Add 6.4, Router 객체에 대한 기본 이해 및 라우팅 분리

--- Chapter6/codes/learn-express/routes/users.js
+++ Chapter6/codes/learn-express/routes/users.js
@@ -5,7 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+/*
+router.get('/users/:id', function(req,res){
+  console.log(req.params, req.query);
+});
+*/
 
+/* connect-flash 미들웨어 테스트 목적
 router.get('/flash', function(req,res){
   req.session.message = '세션 메시지';
   req.flash('message', 'flash 메시지');
@@ -15,5 +21,6 @@ router.get('/flash', function(req,res){
 router.get('/flash/result', function(req,res){
   res.send(`${req.session.message} ${req.flash('message')}`);
 });
+*/
 
 module.exports = router;
